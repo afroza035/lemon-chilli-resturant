e79a7dcaefd02516604e18e6600ce380f2e5fc57
nayem1816 <2191081070@uttarauniversity.edu.bd>
2021-06-12 14:48:52 +0600

    some

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HomeHeader from '../HomeHeader/HomeHeader';
+import OurStory from '../OurStory/OurStory';
 
 const Home = () => {
     return (
         <div>
             <HomeHeader></HomeHeader>
+            <OurStory></OurStory>
         </div>
     );
 };
--- src/components/Home/OurStory/OurStory.js
+++ src/components/Home/OurStory/OurStory.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const OurStory = () => {
-    return (
-        <div>
-            <h2>OurStory</h2>
-        </div>
-    );
+  return (
+    <div className="row p-0 m-0">
+      <div className="d-sm-none col-md-6 d-md-block">
+        <h2>img</h2>
+      </div>
+      <div className="col-sm-12 col-md-6">
+        <h2>text</h2>
+      </div>
+    </div>
+  );
 };
 
 export default OurStory;
